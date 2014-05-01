6af2439bec94a062e4cb5a8b6adbe94d2858b39e
Øyvind Eide <oei011@webmail.uib.no>
2014-05-01 03:39:39 +0200

    debug..

--- package.js
+++ package.js
@@ -6,6 +6,6 @@ Package.on_use(function (api) {
 	if(api.export) {
 		api.export('WaveSurfer');
 	}
-	api.add_files('lib/src/wavesurfer.js', 'client');
+	api.add_files(['lib/src/wavesurfer.js', 'lib/src/drawer.canvas.js', 'lib/src/drawer.js', 'lib/src/webaudio.buffer.js', 'lib/src/webaudio.js', 'lib/src/webaudio.media.js'], 'client');
 	api.add_files('export-wavesurfer.js', 'client');
 });
